4bb346cb1c58daaee03026f500e5d43e9a94faef
Ayush Gour <ayushofficials2609@gmail.com>
2025-03-20 18:12:12 +0530

    latest update v1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import profile from "../assets/profile.png";
 import CopyEmailButton from "./CopyEmailButton";
 import { getConfigData } from "../data/configReader";
+import ResumeDownload from '../assets/Ayush-Gour-SDE.pdf';
 
 export default function Profile() {
   const configData = getConfigData();
@@ -9,14 +10,16 @@ export default function Profile() {
   const workStatusClass = configData.status == "on" ? "w-1.5 h-1.5 bg-[#109d5c] rounded-full" : "w-1.5 h-1.5 bg-[#f74d4d] rounded-full"
   const workStatusText = configData.status == "on" ? "avaılable for work" : "busy"
 
-  // const handleDownload = () => {
-  //   const link = document.createElement("a");
-  //   link.href = Resume;
-  //   link.download = "Resume.pdf";
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // };
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/Ayush-Gour-SDE.pdf"; // Path to the file
+    link.download = "Ayush-Gour-SDE.pdf"; // File name for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
+  
   return (
     <>
       <div className="flex items-center justify-between px-7 py-7">
@@ -60,6 +63,28 @@ export default function Profile() {
               </svg>
               Hire Me
             </button></a>
+
+            <button
+              onClick={()=>{handleDownload()}}
+              type="button"
+              className="gap-x-1 before:ease relative overflow-hidden border border-[#000000] bg-[#050708] text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40 font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center mr-2 mb-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                />
+              </svg>
+              Download Resume
+            </button>
             <CopyEmailButton />
           </div>
         </div>
